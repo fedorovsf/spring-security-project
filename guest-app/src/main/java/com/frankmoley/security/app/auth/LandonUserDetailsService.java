9a3e12a04a99fe7a9683850d6722387b2d89eda9
Sergei Fedorov <fedorovs@inbox.ru>
2019-01-16 12:44:22 +0300

    adding jdbc authentication

---
+++ guest-app/src/main/java/com/frankmoley/security/app/auth/LandonUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.frankmoley.security.app.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LandonUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public LandonUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username);
+
+        if (user == null){
+            throw new UsernameNotFoundException("cannot find username: " + username);
+        }
+
+        return new LandonUserPrincipal(user);
+    }
+}
